7d1afe788c63ffbb67c118a2e26d4c0465631615
𝐌𝐀𝐊𝐈 𝐇𝐀𝐑𝐔𝐊𝐀𝐖𝐀 <makiharukawa.bot@gmail.com>
2026-01-08 15:34:38 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  XAI_API_KEY: 'TU_CLAVE_API_AQUI', // x.ai
+  OPENWEATHER_API_KEY: 'TU_OPENWEATHER_KEY', // openweathermap.org
+  UNSPLASH_API_KEY: 'TU_UNSPLASH_KEY', // unsplash.com/developers
+  OWNER_NAME: '𝓐',
+  OWNER_NUMBER: '573107400303@s.whatsapp.net',
+  BOT_NAME: '𝐌𝐀𝐊𝐈 𝐇𝐀𝐑𝐔𝐊𝐀𝐖𝐀',
+  CHANNEL_URL: 'https://whatsapp.com/channel/0029VbBVZGQ35fM3tALLmF2k',
+  MENU_IMAGE: 'https://i.pximg.net/img-original/img/2024/06/09/00/00/00/119803335_p0.jpg'
+};
