0c730dc23bc1d361d7a916cb6c22a05de5290004
𝐌𝐀𝐊𝐈 𝐇𝐀𝐑𝐔𝐊𝐀𝐖𝐀 <makiharukawa.bot@gmail.com>
2026-01-10 14:59:57 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,30 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-
-const USERS_FILE = path.resolve('./users.json');
-const GROUPS_FILE = path.resolve('./groups.json');
-
-function saveData(users, groups) {
-  try {
-    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
-    fs.writeFileSync(GROUPS_FILE, JSON.stringify(groups, null, 2));
-  } catch (err) {
-    console.warn('saveData error:', err.message);
-  }
-}
-
-function loadData() {
-  try {
-    const users = fs.existsSync(USERS_FILE) ? JSON.parse(fs.readFileSync(USERS_FILE, 'utf8')) : {};
-    const groups = fs.existsSync(GROUPS_FILE) ? JSON.parse(fs.readFileSync(GROUPS_FILE, 'utf8')) : {};
-    return { users, groups };
-  } catch (err) {
-    console.warn('loadData error:', err.message);
-    return { users: {}, groups: {} };
-  }
-}
-
 module.exports = {
-  saveData,
-  loadData
+  saveData(users, groups) {
+    fs.writeFileSync('./users.json', JSON.stringify(users, null, 2));
+    fs.writeFileSync('./groups.json', JSON.stringify(groups, null, 2));
+  },
+  loadData() {
+    let users = fs.existsSync('./users.json') ? JSON.parse(fs.readFileSync('./users.json', 'utf8')) : {};
+    let groups = fs.existsSync('./groups.json') ? JSON.parse(fs.readFileSync('./groups.json', 'utf8')) : {};
+    return { users, groups };
+  }
 };
