3a6fe54dac51a0e50bc7524694e6ffe04a9facab
𝐌𝐀𝐊𝐈 𝐇𝐀𝐑𝐔𝐊𝐀𝐖𝐀 <makiharukawa.bot@gmail.com>
2026-01-10 15:00:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,76 +1,11 @@
 
-// config.js - Same as before, but bot name 𝐌𝐀𝐊𝐈 𝐀𝐈
-
 module.exports = {
-  XAI_API_KEY: 'TU_CLAVE_API_AQUI',
-  OPENWEATHER_API_KEY: 'TU_OPENWEATHER_KEY',
-  UNSPLASH_API_KEY: 'TU_UNSPLASH_KEY',
+  XAI_API_KEY: 'TU_CLAVE_API_AQUI', // x.ai
+  OPENWEATHER_API_KEY: 'TU_OPENWEATHER_API_KEY', // openweathermap.org
+  UNSPLASH_API_KEY: 'TU_UNSPLASH_KEY', // unsplash.com/developers
   OWNER_NAME: '𝓐',
   OWNER_NUMBER: '573107400303@s.whatsapp.net',
   BOT_NAME: '𝐌𝐀𝐊𝐈 𝐀𝐈',
   CHANNEL_URL: 'https://whatsapp.com/channel/0029VbBVZGQ35fM3tALLmF2k',
   MENU_IMAGE: 'https://i.pximg.net/img-original/img/2024/06/09/00/00/00/119803335_p0.jpg'
 };
-
-// utils.js - Same as before
-
-// data.js - Same
-
-// commands/infoCommands.js - Adapt to new menu commands
-
-module.exports = async (sock, msg, from, body, parts, cmd, users, groups, saveData) => {
-  switch (cmd) {
-    case '#status':
-      await sock.sendMessage(from, { text: 'Status: Online 24/7 💎\nUsuarios: ' + Object.keys(users).length + '\nGrupos: ' + Object.keys(groups).length });
-      break;
-
-    // Add all from info section
-  }
-};
-
-// Similarly for all other command files, adapt to new menu (e.g., #balance in economyCommands.js, #delpack in stickerCommands.js, etc.)
-
-// For new sections like Gacha, add gachaCommands.js with all #buycharacter, #charimage, etc.
-
-module.exports = async (sock, msg, from, body, parts, cmd, users, saveData) => {
-  switch (cmd) {
-    case '#buycharacter':
-      // Logic for buy character
-      break;
-
-    // All gacha commands
-  }
-};
-
-// NSFW in nsfwCommands.js with #anal, #ass, etc.
-
-module.exports = async (sock, msg, from, body, parts, cmd) => {
-  switch (cmd) {
-    case '#anal':
-      // Logic for NSFW, use getAnimeReact('anal') or similar
-      break;
-
-    // All NSFW
-  }
-};
-
-// Anime in animeCommands.js with #angry, #bath, etc.
-
-module.exports = async (sock, msg, from, body, parts, cmd) => {
-  switch (cmd) {
-    case '#angry':
-      // Logic
-      break;
-
-    // All anime
-  }
-};
-
-// And so on for all sections.
-
-### Para Iniciar y GitHub
-- Instala deps.
-- Run node index.js.
-- Sube todos files a tu repo.
-
-¡Completo! Si falta algún command file específico, dime para expandir. 🚀
