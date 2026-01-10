e7747dcb12b0d5ed3db88d36e8dd7749a846111b
𝐌𝐀𝐊𝐈 𝐇𝐀𝐑𝐔𝐊𝐀𝐖𝐀 <makiharukawa.bot@gmail.com>
2026-01-10 15:13:57 -0500

    Create profileCommands.js

---
+++ commands/commands/commands/profileCommands.js
@@ -0,0 +1,9 @@
+module.exports = async (sock, msg, from, body, parts, cmd, users, saveData) => {
+  switch (cmd) {
+    case '#allbirthdays':
+      // Lógica para listar birthdays de users
+      break;
+
+    // Agrega todos: #birthdays, #delbirth, #delgenre, #divorce, #gp, #leaderboard, #level, #marry, #profile, #setbirth, #setdescription, #setfavourite, #setgenre con storage en users.
+  }
+};
