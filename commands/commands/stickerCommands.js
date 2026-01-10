55e16f1c7f65d55fc87134fbad1c1a423d0e4dc4
𝐌𝐀𝐊𝐈 𝐇𝐀𝐑𝐔𝐊𝐀𝐖𝐀 <makiharukawa.bot@gmail.com>
2026-01-10 15:05:48 -0500

    Delete commands/commands directory

--- commands/commands/stickerCommands.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (sock, msg, from, body, parts, cmd) => {
-  switch (cmd) {
-    case '#delpack':
-      // Logic to delete pack
-      break;
-
-    // Implement all sticker commands from menu
-  }
-};
