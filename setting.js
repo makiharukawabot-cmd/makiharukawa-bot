e268f6a57913583a4b84a41ffc9d5cbfc41cce69
𝐌𝐀𝐊𝐈 𝐇𝐀𝐑𝐔𝐊𝐀𝐖𝐀 <makiharukawa.bot@gmail.com>
2026-02-05 21:52:08 -0500

    Actualizar setting.js

--- setting.js
+++ setting.js
@@ -1,1 +1,38 @@
-j
+import fs from 'fs';
+
+global.owner = ['573107400303']
+global.botNumber = ''
+
+global.sessionName = 'Sessions/Owner'
+global.version = '^2.0 - Latest'
+global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ Aᴀʀᴏᴍ"
+global.links = {
+api: 'https://api.stellarwa.xyz',
+web: 'http://localhost:5010',
+channel: "https://whatsapp.com/channel/0029VbBVZGQ35fM3tALLmF2k",
+github: "https://github.com/makiharukawabot-cmd/makiharukawa-bot",
+gmail: "makiharukawa.bot@gmail.com"
+}
+global.my = {
+ch: '120363401404146384@newsletter',
+name: 'ೃ࿔ 𝐌𝐀𝐊𝐈 𝐇𝐀𝐑𝐔𝐊𝐀𝐖𝐀 .ೃ࿐',
+}
+
+global.mess = {
+socket: '《✧》 Este comando solo puede ser ejecutado por un Socket.',
+admin: '《✧》 Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+botAdmin: '《✧》 Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
+}
+
+global.APIs = {
+adonix: { url: "https://api-adonix.ultraplus.click", key: "Yuki-WaBot" },
+vreden: { url: "https://api.vreden.web.id", key: null },
+nekolabs: { url: "https://api.nekolabs.web.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+stellar: { url: "https://api.stellarwa.xyz", key: "YukiWaBot", key2: '1bcd4698ce6c75217275c9607f01fd99' },
+apifaa: { url: "https://api-faa.my.id", key: null },
+xyro: { url: "https://api.xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null }
+}
